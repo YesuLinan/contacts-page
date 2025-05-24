f471f97c6ae8f1dce79ffd5af66bd417c7093f94
Jesus Linan <yesudevstudio@gmail.com>
2025-05-23 23:15:30 -0600

    PING

--- api/routes/contactRoutes.js
+++ api/routes/contactRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/ping", (req, res) => {
+  res.status(200).json({ message: "Pong!" });
+});
+
 // Create a new contact
 router.post("/", async (req, res) => {
   try {
